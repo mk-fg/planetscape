c75ccb55c73d70874f6441693859e227f9682b9a
Mike Kazantsev <mk.fraggod@gmail.com>
2010-09-30 16:53:44 +0000

    +twisted

--- planetscape.py
+++ planetscape.py
@@ -2,20 +2,46 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, print_function
 
-import itertools as it, operator as op, functools as ft
-
 import logging
 log = logging.getLogger()
 
 
+
+import itertools as it, operator as op, functools as ft
+
+from twisted.internet.task import LoopingCall
+from twisted.internet import reactor, defer
+from twisted.python import log, failure
+
+
+from twisted.internet import protocol
+
+## TODO: check out if scapy (or something else?) does a better job
+class Traceroute(protocol.ProcessProtocol):
+	def connectionMade(self): self.transport.closeStdin()
+	def processEnded(self, stats):
+		if stats.value.exitCode: self.transport.sentinel.errback(False)
+		else: self.transport.sentinel.callback(True)
+
+
 class PlanetScape(object):
 	'One big TODO now ;)'
+
+	def __init__(self): pass
+	def render(self): pass
+
 
 
 if __name__ == '__main__':
 	from optparse import OptionParser
 	parser = OptionParser(usage='%prog [options]',
 		description='Render stuff on xplanet and run some hooks')
+
+	parser.add_option('-1', '--oneshot', action='store_true',
+		help='Generate single image with a complete set of traces and exit.')
+	parser.add_option('-r', '--refresh', action='store', type='int',
+		default=60, help='Image refresh or re-generate rate. Default: %default.')
+
 	parser.add_option('--debug', action='store_true',
 		help='Give extra info on whats going on.')
 	optz,argz = parser.parse_args()
@@ -24,4 +50,9 @@ if __name__ == '__main__':
 	logging.basicConfig( level=logging.DEBUG
 		if optz.debug else logging.WARNING )
 
-	run_teh_loop()
+	if not optz.oneshot:
+		planescape = PlaneScape()
+		render_task = LoopingCall(optz.refresh, planescape.render)
+		reactor.run()
+	else:
+		PlaneScape().render()
